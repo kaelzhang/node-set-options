eacd89c133069e57f58a7395831a45ee58307614
Kael <i@kael.me>
2015-12-19 17:07:28 +0800

    in progress

--- test/set-options.js
+++ test/set-options.js
@@ -1,23 +1,9 @@
 'use strict';
 
-var expect = require('chai').expect;
-var set_options = require('../');
+var expect = require('chai').expect
+var set = require('../')
 
-function a (a, b) {
-  a.a = a.a || b.a
+var _defaults = {
+  a: 1
 }
-
-function b (a, b) {
-  
-}
-
-console.time(1)
-
-var i = 0;
-var len = 100000
-for (; i < len; i ++) {
-  a({a: 1}, {a: 1})
-}
-console.timeEnd(1)
-
 
